c814e78e1454a0500759f1f39abf357d87e845b1
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2026-03-29 14:49:40 +0200

    feat: functional DnD + toggle

--- src/App/Groceries.purs
+++ src/App/Groceries.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import App.Layout as Layout
 import App.Shared as S
-import Data.Array (fold, mapWithIndex)
+import Data.Array (fold, mapWithIndex, (!!))
 import Data.Array as Array
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Array.NonEmpty as NEA
@@ -17,7 +17,11 @@ import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
+import Web.Event.Event as E
 import Web.HTML.Event.DragEvent (DragEvent)
+import Web.HTML.Event.DragEvent as DragEvent
+import Web.UIEvent.MouseEvent (MouseEvent)
+import Web.UIEvent.MouseEvent as MouseEvent
 
 newtype Amount = MkAmount
   { value :: Number
@@ -55,18 +59,13 @@ type DragState a =
   }
 
 type State =
-  { groceries :: Array Grocery
+  { unchecked :: Array Grocery
+  , checked :: Array Grocery
   , dragState :: Maybe (DragState Int)
   }
 
-toggleGrocery :: GroceryId -> State -> State
-toggleGrocery id state =
-  state { groceries = toggleChecked <$> state.groceries }
-  where
-  toggleChecked grocery
-    | grocery.id == id = grocery
-        { checked = not grocery.checked }
-    | otherwise = grocery
+allGroceries :: State -> Array Grocery
+allGroceries { checked, unchecked } = checked <> unchecked
 
 startDrag :: Int -> State -> State
 startDrag index state =
@@ -78,26 +77,40 @@ overDrag index state =
   state { dragState = _ { dragOverItem = index } <$> state.dragState }
 
 endDrag :: State -> State
-endDrag state =
-  state { dragState = Nothing }
+endDrag state = Maybe.fromMaybe withoutDragState do
+  { dragItem, dragOverItem } <- state.dragState
+  foundDragItem <- state.unchecked !! dragItem
+  updatedGroceries <- Array.deleteAt dragItem state.unchecked
+  let
+    insertIndex =
+      if dragOverItem < dragItem then dragOverItem else dragOverItem
+
+  updatedGroceries' <- Array.insertAt insertIndex foundDragItem updatedGroceries
+  pure $ withoutDragState { unchecked = updatedGroceries' }
+  where
+  withoutDragState = state { dragState = Nothing }
 
 updateGroceryAmount :: GroceryId -> Number -> State -> State
 updateGroceryAmount id delta state =
-  state { groceries = updateGrocery <$> state.groceries }
+  state { unchecked = updateGrocery <$> state.unchecked }
   where
   updateGrocery grocery
     | grocery.id == id = grocery
         { amount = updateAmount delta grocery.amount }
     | otherwise = grocery
 
-toggleGroceryChecked :: GroceryId -> State -> State
-toggleGroceryChecked id state =
-  state { groceries = toggleChecked <$> state.groceries }
+toggleGrocery :: GroceryId -> State -> State
+toggleGrocery id state =
+  state
+    { unchecked = Array.filter (not <<< _.checked) groceries
+    , checked = Array.filter _.checked groceries
+    }
   where
   toggleChecked grocery
     | grocery.id == id = grocery
         { checked = not grocery.checked }
     | otherwise = grocery
+  groceries = toggleChecked <$> allGroceries state
 
 dummyGroceries :: Array Grocery
 dummyGroceries =
@@ -134,7 +147,7 @@ dummyGroceries =
   ]
 
 data Action
-  = ToggleGrocery GroceryId
+  = ToggleGrocery GroceryId MouseEvent
   | StartDrag Int DragEvent
   | OverDrag Int DragEvent
   | EndDrag DragEvent
@@ -168,13 +181,13 @@ groceryView dragState index grocery =
       ( join
           [ [ HP.class_ $ H.ClassName "no-list-style"
             , HP.id $ printGroceryId grocery.id
+            , HE.onClick $ ToggleGrocery grocery.id
             ]
           , case grocery.checked of
               false ->
                 [ HP.draggable true
                 , HE.onDragStart $ StartDrag index
                 , HE.onDragOver $ OverDrag index
-                -- , HP.onDragleave' DragLeaveOccurred
                 ]
               true -> []
           ]
@@ -243,7 +256,8 @@ component =
   where
   initialState :: input -> State
   initialState _ =
-    { groceries: dummyGroceries
+    { unchecked: Array.filter (not <<< _.checked) dummyGroceries
+    , checked: Array.filter _.checked dummyGroceries
     , dragState: Nothing
     }
 
@@ -252,13 +266,15 @@ component =
      . Action
     -> H.HalogenM State Action childSlots output m Unit
   handleAction = case _ of
-    StartDrag index dragEvent ->
+    StartDrag index _dragEvent ->
       H.modify_ $ startDrag index
-    OverDrag index dragEvent ->
+    OverDrag index dragEvent -> do
+      H.liftEffect $ E.preventDefault $ DragEvent.toEvent dragEvent
       H.modify_ $ overDrag index
-    EndDrag dragEvent ->
+    EndDrag _dragEvent -> do
       H.modify_ $ endDrag
-    ToggleGrocery index ->
+    ToggleGrocery index mouseEvent -> do
+      H.liftEffect $ E.preventDefault $ MouseEvent.toEvent mouseEvent
       H.modify_ $ toggleGrocery index
 
   render :: State -> H.ComponentHTML Action () m
@@ -275,7 +291,7 @@ component =
             , HH.button [ HP.class_ $ H.ClassName "secondary" ]
                 [ HH.text "Edit" ]
             ]
-        , case NEA.fromArray state.groceries of
+        , case NEA.fromArray $ allGroceries state of
             Nothing -> HH.text "No groceries have been added yet"
             Just nea -> groceriesView state.dragState nea
         , HH.button [ HP.class_ $ H.ClassName "fab" ] [ HH.text "+" ]
