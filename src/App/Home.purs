13a6628fd66f9e28f6d30156b7d701af3caca6c3
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2026-07-04 21:56:02 +0200

    feat: form field state space id completed in Home component

--- src/App/Home.purs
+++ src/App/Home.purs
@@ -2,16 +2,19 @@ module App.Home where
 
 import Prelude
 
-import App.FormField (FormField)
+import App.FormField (FormField(..))
 import App.FormField as FormField
 import App.Layout as Layout
 import App.Shared (eventTargetInputValue, preventDefault)
 import Capabilities.Resource.ManageSpaces (class ManageSpaces)
 import Capabilities.Resource.ManageSpaces as ManageSpaces
+import Data.Either (Either(..))
 import Data.Either as Either
 import Data.Maybe (Maybe(..))
 import Data.String.NonEmpty as NonEmptyString
+import Data.Traversable (traverse_)
 import Data.ULID as DULID
+import Debug as Debug
 import Domain.Id as Id
 import Domain.Space (Space)
 import Domain.SpaceId (SpaceId)
@@ -22,7 +25,6 @@ import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Partial.Unsafe (unsafeCrashWith)
 import Web.Event.Event (Event)
-import Web.UIEvent.InputEvent (InputEvent)
 
 theSpaceId :: SpaceId
 theSpaceId =
@@ -50,6 +52,17 @@ data Action
   | ClickedAccept Event
   | ClickedSelect Space
   | SpaceIdChanged Event
+
+updateSpaceIdField :: String -> State -> State
+updateSpaceIdField spaceIdCandidate = case _ of
+  NotInitialized -> NotInitialized
+  Initialized state ->
+    Initialized $ state { spaceIdField = updatedSpaceIdField }
+    where
+    updatedSpaceIdField =
+      case DULID.parse spaceIdCandidate of
+        Left _ -> FormField.Invalid spaceIdCandidate
+        Right _ -> FormField.Valid spaceIdCandidate
 
 spaceView :: forall m. Space -> H.ComponentHTML Action () m
 spaceView space =
@@ -99,6 +112,7 @@ component =
 
     SpaceIdChanged event -> do
       spaceIdValue <- eventTargetInputValue event
+      traverse_ (H.modify_ <<< updateSpaceIdField) spaceIdValue
 
   render :: State -> H.ComponentHTML Action () m
   render state =
@@ -110,18 +124,43 @@ component =
             [ HH.h1_ [ HH.text "What's for dinner" ]
             , HH.h2_ [ HH.text "Space shared with you" ]
             , HH.form [ HE.onSubmit ClickedAccept ]
-                [ HH.fieldset [ HP.attr (H.AttrName "role") "group" ]
-                    [ HH.input
-                        [ HP.type_ HP.InputText
-                        , HP.name "space-id"
-                        , HP.placeholder "Enter space id"
-                        , HE.onChange SpaceIdChanged
+                [ HH.fieldset
+                    ( join
+                        [ [ HP.attr (H.AttrName "role") "group" ]
+                        , FormField.ariaValidity
+                            initializedState.spaceIdField
                         ]
-                    , HH.input [ HP.type_ HP.InputSubmit, HP.value "Accept" ]
+                    )
+                    [ HH.input
+                        ( join
+                            [ [ HP.type_ HP.InputText
+                              , HP.name "space-id"
+                              , HP.placeholder "Enter space id"
+                              , HE.onChange SpaceIdChanged
+                              ]
+                            , FormField.ariaValidity
+                                initializedState.spaceIdField
+                            ]
+                        )
+                    , HH.input
+                        [ HP.type_ HP.InputSubmit
+                        , HP.value "Accept"
+                        , HP.disabled $ isAcceptDisabled initializedState
+                        ]
+                    ]
+                , HH.small [ HP.class_ $ H.ClassName "white-space-pre-wrap" ]
+                    [ case initializedState.spaceIdField of
+                        Invalid _ -> HH.text "Please enter a valid space id"
+                        _ -> HH.text " "
                     ]
                 ]
             , HH.h2_ [ HH.text "Saved spaces" ]
             , HH.ul [ HP.class_ $ H.ClassName "no-padding select-list" ] $
                 map spaceView initializedState.spaces
             ]
+    where
+    isAcceptDisabled initializedState =
+      case initializedState.spaceIdField of
+        Invalid _ -> true
+        _ -> false
 
