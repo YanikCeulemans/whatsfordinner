21e4994de98de6c020b5c2f9a7f3c545256af36c
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2026-07-19 15:04:34 +0200

    WIP: web socket in httpurple

--- packages/api/src/Main.purs
+++ packages/api/src/Main.purs
@@ -3,18 +3,24 @@ module Api.Main where
 import HTTPurple
 import Prelude hiding ((/))
 
-data Route = Api String
+data Route
+  = Api String
+  | WS
 
 derive instance Generic Route _
 
 route :: RouteDuplex' Route
 route = mkRoute
   { "Api": "api" / segment
+  , "WS": "ws" / noArgs
   }
 
 main :: ServerM
 main = do
   serve { port: 8080 } { route, router }
   where
-  router { route: Api rest } = ok $ "api route " <> rest
+  router = case _ of
+    { route: Api rest } -> ok $ "api route " <> rest
+    { route: WS, headers } -> switchingProtocols' $ headers
+      { "Sec-WebSocket-Accept": lookup headers "Sec-WebSocket-Key" }
 
