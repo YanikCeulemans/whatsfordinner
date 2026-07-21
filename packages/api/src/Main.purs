8684d9137e845f2b9f580fdf07453918565f453f
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2026-07-21 12:57:12 +0200

    feat: low level response handling in custom middleware

--- packages/api/src/Main.purs
+++ packages/api/src/Main.purs
@@ -3,7 +3,9 @@ module Api.Main where
 import HTTPurple
 import Prelude hiding ((/))
 
+import Data.Array as Array
 import Data.Maybe (Maybe(..))
+import Data.String as String
 import Debug as Debug
 import Effect (Effect)
 import Effect.Aff.Compat
@@ -16,8 +18,12 @@ import Effect.Aff.Compat
 import Effect.Exception (Error)
 import Effect.Exception.Unsafe (unsafeThrow)
 import Literals.Undefined (undefined)
+import Node.Encoding as Encoding
 import Node.HTTP.IncomingMessage as NodeIM
+import Node.HTTP.OutgoingMessage as OutgoingMessage
+import Node.HTTP.ServerResponse as ServerResponse
 import Node.HTTP.Types (IMServer, IncomingMessage, ServerResponse)
+import Node.Stream as Writable
 import Untagged.Union (UndefinedOr, asOneOf)
 
 data Route
@@ -58,11 +64,36 @@ pursMiddleware middleware = NodeMiddleware $ mkEffectFn3 help
     middleware request response $ next' next
     pure $ pure unit
 
+parseUrlParts :: String -> Array String
+parseUrlParts =
+  String.split (String.Pattern "/")
+    >>> Array.filter (not <<< String.null)
+
 myMiddleware :: PursMiddleware
 myMiddleware request response next = do
-  socket <- NodeIM.socket request
-  Debug.traceM { socket }
-  next $ asOneOf undefined
+  let
+    method = NodeIM.method request
+    url = NodeIM.url request
+    urlParts = parseUrlParts url
+  Debug.traceM { method, url, urlParts }
+  case method, urlParts of
+    "GET", [ "ws" ] -> do
+      socket <- NodeIM.socket request
+      Debug.traceM { socket, method, url }
+      ServerResponse.setStatusCode 200 response
+      let
+        payload = "Hello, world!"
+        outMsg = ServerResponse.toOutgoingMessage response
+      OutgoingMessage.appendHeader "Content-Type" "text/plain" outMsg
+      OutgoingMessage.appendHeader
+        "Content-Length"
+        (show $ String.length payload)
+        outMsg
+      let
+        writeable = OutgoingMessage.toWriteable outMsg
+      void $ Writable.writeString writeable Encoding.UTF8 payload
+    _, _ ->
+      next $ asOneOf undefined
 
 nodeMiddleware :: NodeMiddlewareStack () ()
 nodeMiddleware =
