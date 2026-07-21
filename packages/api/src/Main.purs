ab9f7239f83cc7911266d0cd9aad941b406cee60
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2026-07-21 12:56:03 +0200

    feat: purs middleware out of node middleware

--- packages/api/src/Main.purs
+++ packages/api/src/Main.purs
@@ -3,6 +3,23 @@ module Api.Main where
 import HTTPurple
 import Prelude hiding ((/))
 
+import Data.Maybe (Maybe(..))
+import Debug as Debug
+import Effect (Effect)
+import Effect.Aff.Compat
+  ( EffectFn1
+  , EffectFn3
+  , mkEffectFn1
+  , mkEffectFn3
+  , runEffectFn1
+  )
+import Effect.Exception (Error)
+import Effect.Exception.Unsafe (unsafeThrow)
+import Literals.Undefined (undefined)
+import Node.HTTP.IncomingMessage as NodeIM
+import Node.HTTP.Types (IMServer, IncomingMessage, ServerResponse)
+import Untagged.Union (UndefinedOr, asOneOf)
+
 data Route
   = Api String
   | WS
@@ -15,12 +32,49 @@ route = mkRoute
   , "WS": "ws" / noArgs
   }
 
+handleWS :: RequestHeaders -> ResponseM
+handleWS reqHeaders =
+  case lookup reqHeaders wsKeyKey of
+    Nothing -> badRequest $ "missing websocket key header: " <> wsKeyKey
+    Just wsKey ->
+      switchingProtocols' $ headers
+        { "Sec-WebSocket-Accept": wsKey }
+  where
+  wsKeyKey = "Sec-WebSocket-Key"
+
+type PursMiddleware =
+  (IncomingMessage IMServer)
+  -> ServerResponse
+  -> (UndefinedOr Error -> Effect Unit)
+  -> Effect Unit
+
+pursMiddleware
+  :: PursMiddleware
+  -> NodeMiddleware ()
+pursMiddleware middleware = NodeMiddleware $ mkEffectFn3 help
+  where
+  next' n = runEffectFn1 n
+  help request response next = do
+    middleware request response $ next' next
+    pure $ pure unit
+
+myMiddleware :: PursMiddleware
+myMiddleware request response next = do
+  socket <- NodeIM.socket request
+  Debug.traceM { socket }
+  next $ asOneOf undefined
+
+nodeMiddleware :: NodeMiddlewareStack () ()
+nodeMiddleware =
+  NodeMiddlewareStack
+    $ usingMiddleware
+    $ pursMiddleware myMiddleware
+
 main :: ServerM
 main = do
-  serve { port: 8080 } { route, router }
+  serveNodeMiddleware { port: 8080 } { route, router, nodeMiddleware }
   where
   router = case _ of
     { route: Api rest } -> ok $ "api route " <> rest
-    { route: WS, headers } -> switchingProtocols' $ headers
-      { "Sec-WebSocket-Accept": lookup headers "Sec-WebSocket-Key" }
+    { route: WS, headers } -> handleWS headers
 
